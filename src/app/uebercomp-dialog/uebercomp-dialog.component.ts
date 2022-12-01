3ecd79877a84006f6c5a32275dd8ad24a15f2d76
WenzelSSE <wenzel@sse.uni-hildesheim.de>
2022-12-01 10:35:48 +0100

    change creation of Übercomp to fit new Swagger Client

--- src/app/uebercomp-dialog/uebercomp-dialog.component.ts
+++ src/app/uebercomp-dialog/uebercomp-dialog.component.ts
@@ -1,17 +1,75 @@
-import { Component, Inject } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { UeberCompetenceCreationDto } from 'competence_repository_api_typescript-angular';
+import { AuthenticationService, CompetenceDto, CompetenciesService, ResolvedUeberCompetenceDto, UeberCompetenceCreationDto } from 'competence_repository_api_typescript-angular';
+import { LoDialogComponent } from '../lo-dialog/lo-dialog.component';
 
 @Component({
   selector: 'app-uebercomp-dialog',
   templateUrl: './uebercomp-dialog.component.html',
   styleUrls: ['./uebercomp-dialog.component.scss']
 })
-export class UebercompDialogComponent {
-  constructor(
+export class UebercompDialogComponent implements OnInit{
+
+  competencies: CompetenceDto[] = [];
+  ueber_competencies: ResolvedUeberCompetenceDto[] = [];
+
+  displayedColumns = ["id", "level", "skill", "select"];
+  displayedColumnsUeber = ["id", "name", "desc", "select"];
+
+  selectionComp = new SelectionModel<CompetenceDto>(true, []);
+  selectionUeberComp = new SelectionModel<ResolvedUeberCompetenceDto>(true, []);
+  constructor(private compService: CompetenciesService,
+    private authService: AuthenticationService,
     public dialogRef: MatDialogRef<UebercompDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: UeberCompetenceCreationDto,
   ) {}
+  ngOnInit(): void {
+    this.compService.configuration.accessToken = this.authService.configuration.accessToken
+    this.getCompetencies();
+  }
+
+  getCompetencies(): void {
+    
+    this.compService.repositoryMgmtControllerLoadResolvedRepository('1').subscribe({
+      next: (v) => {
+        console.log(v)
+   
+        this.competencies = v.competencies
+        this.ueber_competencies = v.ueberCompetencies
+
+      },
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    })
+
+  }
+
+  selectHandlerComp(row: CompetenceDto) {
+
+    this.selectionComp.toggle(row);
+    let locArray : Array<string> = [];
+    this.selectionComp.selected.forEach(element => {
+      console.log(element.id)
+      locArray.push(element.id)
+    });
+    console.log(locArray);
+    this.data.nestedCompetencies   = locArray;
+  }
+
+  selectHandlerUeberComp(row: ResolvedUeberCompetenceDto) {
+
+    this.selectionUeberComp.toggle(row);
+
+    let locArray : Array<string> = [];
+    this.selectionUeberComp.selected.forEach(element => {
+      console.log(element.id)
+      locArray.push(element.id)
+    });
+    console.log(locArray);
+    this.data.nestedUeberCompetencies = locArray;
+  }
+
 
   onNoClick(): void {
     this.dialogRef.close();
