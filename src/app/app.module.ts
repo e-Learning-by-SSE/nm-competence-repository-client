60fe24d253ac09569590f2ddd24600db95759e97
WenzelSSE <wenzel@sse.uni-hildesheim.de>
2022-12-09 15:32:00 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ const configurationFactory = () => {
 
 const configurationFactory2 = () => {
   const configParams: Param2 = {
-    basePath: 'http://127.0.0.1:5000',
+    basePath: 'https://staging.sse.uni-hildesheim.de:9012',
 
   };
   return new Config2(configParams);
